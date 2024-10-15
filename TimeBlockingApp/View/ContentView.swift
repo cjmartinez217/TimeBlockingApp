1a3df23c536f387dfa367da139c7ced27d5bd2bb
Christian Martinez <cjmartinez217@gmail.com>
2024-10-15 08:04:53 -0500

    added DayHeaderView

---
+++ TimeBlockingApp/View/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TimeBlockingApp
+//
+//  Created by Christian Martinez on 10/11/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    var body: some View {
+        DayHeaderView()
+        Spacer()
+    }
+}
+
+#Preview {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
---
+++ TimeBlockingApp/View/DayHeaderView.swift
@@ -0,0 +1,31 @@
+//
+//  DayHeaderView.swift
+//  TimeBlockingApp
+//
+//  Created by Christian Martinez on 10/12/24.
+//
+
+import SwiftUI
+
+struct DayHeaderView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "line.horizontal.3")
+                .font(.system(size: 38, weight: .bold, design: .rounded))
+            VStack(alignment: .leading) {
+                Text("Thursday")
+                    .font(.system(size: 26, weight: .medium, design: .rounded))
+                Text("July 25")
+                    .font(.system(size: 18, weight: .light, design: .rounded))
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        DayHeaderView()
+        Spacer()
+    }
+}
