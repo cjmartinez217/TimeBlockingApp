62cf371478861df5af6d2ca744a206a2362aab61
Christian Martinez <cjmartinez217@gmail.com>
2024-11-19 11:20:07 -0600

    started working on Weekly view

--- TimeBlockingApp/View/CalendarViews/WeekCalendarView.swift
+++ TimeBlockingApp/View/CalendarViews/WeekCalendarView.swift
@@ -11,9 +11,21 @@ struct WeekCalendarView: View {
     @Binding var presentSideMenu: Bool
 
     var body: some View {
-        HStack {
-            SideMenuButton(presentSideMenu: $presentSideMenu)
-            Text("Weekly view")
+        VStack {
+            HStack(alignment: .center) {
+                SideMenuButton(presentSideMenu: $presentSideMenu)
+                Text("July")
+                    .font(.system(size: 26, weight: .medium, design: .rounded))
+                Spacer()
+            }
+            .padding(.leading, 10)
+            ZStack(alignment: .top) {
+                Divider()
+                    .background(Color.black)
+                    .padding(.top)
+                    .clipped()
+                WeekTimeGrid()
+            }
         }
     }
 }
